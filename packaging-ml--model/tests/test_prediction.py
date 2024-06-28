b5ac5b8a9ecdb726f680cd0c5061729dc6b80f61
Ayush04H <ayush050419@gmail.com>
2024-06-28 13:54:28 +0530

    Version WIth new tests

--- packaging-ml--model/tests/test_prediction.py
+++ packaging-ml--model/tests/test_prediction.py
@@ -16,7 +16,7 @@ from prediction_model.predict import generate_predictions
 # output from predict script is str data type
 # the output is Y for an example data
 
-#Fixtures --> functions before test function --> ensure single_prediction
+#Defining Fixtures --> functions before test function --> ensure single_prediction
 
 @pytest.fixture
 def single_prediction():
@@ -28,8 +28,8 @@ def single_prediction():
 def test_single_pred_not_none(single_prediction): # output is not none
     assert single_prediction is not None
 
-def test_single_pred_str_type(single_prediction): # data type is string
+def test_single_pred_str_type(single_prediction): # data type is string or not
     assert isinstance(single_prediction.get('prediction')[0],str)
 
-def test_single_pred_validate(single_prediction): # check the output is Y
+def test_single_pred_validate(single_prediction): # Accpets only Y
     assert single_prediction.get('prediction')[0] == 'Y'
