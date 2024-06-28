cc7ea3923bf21e7215af02aee0c205aceffd2f55
Ayush04H <ayush050419@gmail.com>
2024-06-28 13:57:14 +0530

    Compatible Versions with Tests

--- packaging-ml--model/prediction_model/predict.py
+++ packaging-ml--model/prediction_model/predict.py
@@ -14,22 +14,20 @@ from prediction_model.processing.data_handling import load_pipeline,load_dataset
 
 classification_pipeline = load_pipeline(config.MODEL_NAME)
 
-#def generate_predictions(data_input):
-#    data = pd.DataFrame(data_input)
-#    pred = classification_pipeline.predict(data[config.FEATURES])
- #   output = np.where(pred==1,'Y','N')
-#    result = {"prediction":output}
-#    return result
+def generate_predictions(data_input):
+    data = pd.DataFrame(data_input)
+    pred = classification_pipeline.predict(data[config.FEATURES])
+    output = np.where(pred==1,'Y','N')
+    result = {"prediction":output}
+    return result
 
-
-#New Test Function
-def generate_predictions():
-     test_data = load_dataset(config.TEST_FILE)
-     pred = classification_pipeline.predict(test_data[config.FEATURES])
-     output = np.where(pred==1,'Y','N')
-     print(output)
-     #result = {"Predictions":output}
-     return output
+# def generate_predictions():
+#     test_data = load_dataset(config.TEST_FILE)
+#     pred = classification_pipeline.predict(test_data[config.FEATURES])
+#     output = np.where(pred==1,'Y','N')
+#     print(output)
+#     #result = {"Predictions":output}
+#     return output
 
 if __name__=='__main__':
     generate_predictions()
